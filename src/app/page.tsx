0f97c8cffbf9ba82622656a0c5f78ba69f8d5667
Yuri Diogo <yuri.diogo.az@gmail.com>
2024-03-21 16:23:05 -0300

    remove hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import BackgroundImage from '@/components/background-image';
 import Champions from '@/components/champions';
-import HeroImage from '@/components/hero-image';
 
 export default function Home() {
   return (
     <main className="h-screen w-full">
       <BackgroundImage />
 
-      <HeroImage />
       <Champions />
     </main>
   );
